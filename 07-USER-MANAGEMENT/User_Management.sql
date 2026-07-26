a54386f1bfff6f4f289e36f18d21b3477151aafe
MRITUNJAY KUMAR <mritunjay1333@gmail.com>
2026-07-26 13:42:50 +0530

    Add files via upload

---
+++ 07-USER-MANAGEMENT/User_Management.sql
@@ -0,0 +1,25 @@
+--Create user--
+CREATE USER c##mritunjay
+identified BY <sidhut5911>;
+ 
+ --granting permission--
+  GRANT connect,resource,unlimited tablespace
+  TO <c##mritunjay>;
+   
+   --conncet  (allow users to  login)
+   --resource (allow users to create database objects i.e.. tables,views,indexs,procedures)
+   --unlimited tablespace (allow user to insert records)
+   
+   --modify system password__
+    --login as SYS as SYS dba
+	--then don't enter password/just enter enter key
+	
+  ALTER USER system
+  identified BY 12345;
+  -- user altered--
+   
+   -- modify user password
+    --login as system [user]/password[at the time of installing]
+	  ALTER USER c##mritunjay
+	  identified BY mritunjay5911;
+	  -- user altered
